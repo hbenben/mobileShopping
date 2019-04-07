2b6097e89da6a5698314ee06cb63903f5fe07f37
hbb <375152092@qq.com>
2019-04-07 19:08:09 +0800

    13天剑zepto的动态成员，全局可以使用

--- js/common.js
+++ js/common.js
@@ -1,15 +1,15 @@
-$(function(){
+$(function () {
   // zepto里面的拦截器
   //使用zepto里面的ajax中的beforeSend来实现路由的拼接
   // 定义一个前缀相同的路由
   var baseURL = 'http://157.122.54.189:9094/api/public/v1/';
   // 添加zepto里面的拦截器，让每一个ajax请求都经过这个函数处理加工
-  $.ajaxSettings.beforeSend=function(xhr,obj){
+  $.ajaxSettings.beforeSend = function (xhr, obj) {
     //调用ajax.Settings里面 的beforeSend方法，里面有两个参数
-  // 可以查看每个ajax中的url请求路径  ，实现url的拼接
+    // 可以查看每个ajax中的url请求路径  ，实现url的拼接
     // console.log(obj);
-    obj.url=baseURL+obj.url;
-    
+    obj.url = baseURL + obj.url;
+
   }
 
   //mui中的点击事件，不使用click而是自己封装tap
@@ -24,4 +24,20 @@ $(function(){
     // 在这边我们想拼接url
     $('body').removeClass('loadding')
   }
+  //先动态扩展zepto成员,在全局使用$.getParameter方法
+  $.extend($,{
+    getParameter:function(url){
+      var obj = {}
+      // location.search:url中?及?后面的内容
+      url = url.substring(1) //cid=5&name=jack
+      // 先按&拆分
+      var arr = url.split('&') //['cid=5','name=jack']
+      // 遍历进行第二次拆分
+      for (var i = 0; i < arr.length; i++) {
+        var temp = arr[i].split('=') //['cid',5]
+        obj[temp[0]] = temp[1] // obj['cid'] = 5
+      }
+      return obj
+    }
+  })
 })
--- js/goodsDetail.js
+++ js/goodsDetail.js
@@ -1,5 +1,5 @@
 $(function () {
-  //获得slider插件对象
+  //获得slider插件对象，自动轮播
   var gallery = mui('.mui-slider');
   gallery.slider({
     interval: 2000 //自动轮播周期，若为0则不自动播放，默认为0；
@@ -9,4 +9,10 @@ $(function () {
   mui('.mui-scroll-wrapper').scroll({
     deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
   });
+
+  // 发送ajax请求
+  $.ajax({
+    
+  })
+
 })
